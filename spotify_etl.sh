61cec7101769dda142f251bdaaf6e68616ed8658
Amaguk2023 <ernestomanuel2023@gmail.com>
2021-07-26 14:54:38 +0200

    Bash File

---
+++ spotify_etl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PROGRAM PATHS FOR PYSPARK TO WORK AND OTHER PYTHON LIBRARIES. TAKEN FROM BASH PROFILE.
+export SPARK_HOME=""
+export PATH=$PATH:$SPARK_HOME/bin
+export PYSPARK_PYTHON=python3
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip:$PYTHONPATH
+export SPARK_LOCAL_IP=localhost
+PATH="/path/to/Python.framework/:${PATH}"
+export PATH
+alias python='python3' 
+
+#PATH TO SCRIPT
+python /path/to/script.py
+
+
