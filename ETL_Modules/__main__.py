bdf0d9ed44ac8ab50f80d0b63d04c91afb42a2f3
Amaguk2023 <ernestomanuel2023@gmail.com>
2021-07-30 18:41:59 +0200

    Program divided in modules

---
+++ ETL_Modules/__main__.py
@@ -0,0 +1,27 @@
+import requests
+import data_request
+
+if __name__ == '__main__':
+
+    ENCODED_CLIENT_ID_CLIENT_SECRET = 'DATA'
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
+    REFRESH_TOKEN = 'DATA'
+
+    #API CREDENTIALS FOR TOKEN REQUEST (RESPONSE 200 MEANS REQUEST ACHIEVED)
+    headers = {
+        "Authorization": "Basic {client_id_client_secret}".format(client_id_client_secret=ENCODED_CLIENT_ID_CLIENT_SECRET)
+    }
+
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": "{token}".format(token=REFRESH_TOKEN)
+    }
+
+    #TOKEN POST REQUEST
+    token_post = requests.post(TOKEN_URL, headers=headers, data=data)
+    token_data = token_post.json()
+    access_token = token_data["access_token"]
+
+    #CONSTRAINTS
+    ACCESS_TOKEN = access_token
+    data_request.spotify_data_request(ACCESS_TOKEN)
---
+++ ETL_Modules/data_request.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from datetime import datetime
+import datetime
+import extract
+
+def spotify_data_request(ACCESS_TOKEN):
+
+	#API CREDENTIALS FOR DATA REQUEST
+	headers = {
+		"Accept" : "application/json",
+		"Content-Type" : "application/json",
+		"Authorization" : "Bearer {token}".format(token=ACCESS_TOKEN)
+	}
+
+	#TODAY/YESTERDAY CREATION. UNIX MILISECONDS
+	today = datetime.datetime.now()
+	yesterday = today - datetime.timedelta(days=1)
+	yesterday_unix = int(yesterday.timestamp() * 1000)
+
+	#REQUEST DATA FROM SPOTIFY API
+	recently_played_data_request = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix), headers=headers) #data file path ADD THE SPOTIFY API FUNCTION HERE
+
+	#MAKE DATA REQUEST JSON FILE
+	recently_played_json = recently_played_data_request.json()
+	extract.data_extraction(recently_played_json)
+
+
+
